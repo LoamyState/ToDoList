6e086d5828da4ea0d01f2c7595e868a63fa8820d
Jane Madsen <janemadsen815@gmail.com>
2023-04-03 09:33:07 -0600

    Complete project as outlined in book

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,51 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by J Madsen on 3/26/23.
+//
+
+import Foundation
+
+struct ToDo: Equatable, Codable {
+    let id: UUID
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    init(title: String, isComplete: Bool, dueDate: Date, notes: String? = nil) {
+        self.id = UUID()
+        self.title = title
+        self.isComplete = isComplete
+        self.dueDate = dueDate
+        self.notes = notes
+    }
+    
+    static let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let archiveURL = documentsDirectory.appendingPathComponent("toDos").appendingPathExtension("plist")
+    
+    static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    static func saveToDos(_ toDos: [ToDo]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let codedToDos = try? propertyListEncoder.encode(toDos)
+        try? codedToDos?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    static func loadToDos() -> [ToDo]? {
+        guard let codedToDos = try? Data(contentsOf: archiveURL) else {return nil}
+        let propertyListDecoder = PropertyListDecoder()
+        return try? propertyListDecoder.decode(Array<ToDo>.self, from: codedToDos)
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let toDo1 = ToDo(title: "ToDo1", isComplete: false, dueDate: Date(), notes: "Notes 1")
+        let toDo2 = ToDo(title: "ToDo2", isComplete: false, dueDate: Date(), notes: "Notes 2")
+        let toDo3 = ToDo(title: "ToDo3", isComplete: false, dueDate: Date(), notes: "Notes 3")
+        
+        return [toDo1, toDo2, toDo3]
+    }
+}
---
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDoList
+//
+//  Created by J Madsen on 3/31/23.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    
+    var toDo: ToDo?
+    
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var dueDateLabel: UILabel!
+    @IBOutlet weak var dueDateDatePicker: UIDatePicker!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    var isDatePickerHidden = true
+    let dateLabelIndexPath = IndexPath(row: 0, section: 1)
+    let datePickerIndexPath = IndexPath(row: 1, section: 1)
+    let notesIndexPath = IndexPath(row: 0, section: 2)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let currentDueDate: Date
+        
+        if let toDo = toDo {
+            navigationItem.title = "To-Do"
+            titleTextField.text = toDo.title
+            isCompleteButton.isSelected = toDo.isComplete
+            currentDueDate = toDo.dueDate
+            notesTextView.text = toDo.notes
+        } else {
+            currentDueDate = Date().addingTimeInterval(24*60*60)
+        }
+        
+        dueDateDatePicker.date = currentDueDate
+        updateDueDateLabel(date: currentDueDate)
+        updateSaveButtonState()
+    }
+    
+    
+    
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        isCompleteButton.isSelected.toggle()
+    }
+    
+
+    func updateSaveButtonState() {
+        let shouldEnableSaveButton = titleTextField.text?.isEmpty == false
+        saveBarButton.isEnabled = shouldEnableSaveButton
+    }
+    
+    @IBAction func textEditingChanged(_ sender: Any) {
+        updateSaveButtonState()
+    }
+    
+    @IBAction func returnPressed(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    func updateDueDateLabel(date: Date) {
+        dueDateLabel.text = date.formatted(.dateTime.month(.defaultDigits).day().year(.twoDigits).hour().minute())
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath == dateLabelIndexPath {
+            isDatePickerHidden.toggle()
+            updateDueDateLabel(date: dueDateDatePicker.date)
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+    
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        updateDueDateLabel(date: sender.date)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath where isDatePickerHidden == true:
+            return 0
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath:
+            return 216
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard segue.identifier == "saveUnwind" else { return }
+        
+        let title = titleTextField.text ?? "Default Title"
+        let isComplete = isCompleteButton.isSelected
+        let dueDate = dueDateDatePicker.date
+        let notes = notesTextView.text
+        
+        if toDo != nil {
+            toDo?.title = title
+            toDo?.isComplete = isComplete
+            toDo?.dueDate = dueDate
+            toDo?.notes = notes
+        } else {
+            toDo = ToDo(title: title, isComplete: isComplete, dueDate: dueDate, notes: notes)
+        }
+    }
+    
+}
